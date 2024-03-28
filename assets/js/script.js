1a480c2fd957d98b6edb4b9605f9e2eb70cebd15
lewis-creates <140522671+lewis-creates@users.noreply.github.com>
2024-03-28 13:47:13 +0000

    Implemented category selection for the quiz, Enabled category selection by adding event listeners to category buttons. Clicking a category button updates the quiz state to reflect the chosen category and reveals the difficulty selection area.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ const startBtn = document.querySelector(".start-btn");
 const instructionsArea = document.querySelector(".instructions-area");
 const beginBtn = document.querySelector(".begin-btn");
 const categoryArea = document.querySelector(".category-area");
+const difficultyArea = document.querySelector(".difficulty-area");
+const gameBtn = document.querySelector(".game-btn");
+const genreBtn = document.querySelector(".genre-btn");
+const hardwareBtn = document.querySelector(".hardware-btn");
+const historyBtn = document.querySelector(".history-btn");
+
+let selectedCategory = '';
 
 startBtn.addEventListener("click", function() {
     instructionsArea.classList.add("active-info");
@@ -10,4 +17,28 @@ startBtn.addEventListener("click", function() {
 beginBtn.Btn.addEventListener("click", function() {
     instructionsArea.classList.remove("active-info");
     categoryArea.classList.add("active-category");
+});
+
+gameBtn.addEventListener("click", function() {
+    selectedCategory = 'game';
+    categoryArea.classList.remove("active-category");
+    difficultyArea.classList.add("active-difficulty");
+});
+
+genreBtn.addEventListener("click", function() {
+    selectedCategory = 'genre';
+    categoryArea.classList.remove("active-category");
+    difficultyArea.classList.add("active-difficulty");
+});
+
+hardwareBtn.addEventListener("click", function() {
+    selectedCategory = 'hardware';
+    categoryArea.classList.remove("active-category");
+    difficultyArea.classList.add("active-difficulty");
+});
+
+historyBtn.addEventListener("click", function() {
+    selectedCategory = 'history';
+    categoryArea.classList.remove("active-category");
+    difficultyArea.classList.add("active-difficulty");
 });
